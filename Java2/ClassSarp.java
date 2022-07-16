1594bec414120958e6a56275dd1f39a3bbe95a36
YoDoIT <sarpsenocak@outlook.com>
2022-07-16 14:40:54 -0400

    this is a git test

---
+++ Java2/ClassSarp.java
@@ -0,0 +1,7 @@
+package Java2;
+
+public class ClassSarp {
+    public static void main(String[] args) {
+        System.out.println("sadasdsada");
+    }
+}
--- JavaPrac1/Class1.java
+++ JavaPrac1/Class1.java
@@ -1,3 +1,5 @@
+package JavaPrac1;
+
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("This is Toronto Office");
